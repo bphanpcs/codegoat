c38b5b87885763ae3e31597b9d2bf622880d8cce
Binh Phan <92009631+bphanpcs@users.noreply.github.com>
2023-03-09 09:41:59 -0800

    Update main.tf
    updated instance size

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ module "iam" {
 module "instance" {
   source        = "terraform-aws-modules/ec2-instance/aws"
   ami           = var.ami
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   name          = "example-server"
 
   vpc_security_group_ids = [module.vpc.vpc_sg_id]
